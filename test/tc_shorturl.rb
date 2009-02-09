cd98cf99728ffc8caa1593aa3cfb38d3e98f78d6
Fabien Jakimowicz <rider@soda.local>
2009-02-09 18:41:45 +0100

    updating test : fetching result urls and follow redirection to match tinysation of each url

--- test/tc_shorturl.rb
+++ test/tc_shorturl.rb
@@ -6,13 +6,9 @@ $test_lib_dir = File.join(File.dirname(__FILE__), "..", "lib")
 $:.unshift($test_lib_dir)
 
 require "test/unit"
+require "net/http"
+require "uri"
 require "shorturl"
-
-class String
-  def url?
-    self[0..6].downcase == "http://"
-  end
-end
 
 class TestShortURL < Test::Unit::TestCase
   def setup
@@ -21,15 +17,27 @@ class TestShortURL < Test::Unit::TestCase
   
   def test_shorten
     # Default service (RubyURL)
-    assert ShortURL.shorten(@url).url?
+    assert_equal @url, fetch_redirection(ShortURL.shorten(@url), @url)
 
-    # All the services (I can't test exact URLs since they seem to
-    # change semi regularly)
+    # All the services
     ShortURL.valid_services.each do |service|
-      assert ShortURL.shorten(@url, service).url?
+      assert_equal @url, fetch_redirection(ShortURL.shorten(@url, service), @url)
     end
     
     # An invalid service
     assert_raise(InvalidService) { ShortURL.shorten(@url, :foobar) }
+  end
+  
+  protected
+  # Follow redirections and returns final url
+  def fetch_redirection(orig, stop = nil)
+    uri = URI.parse(orig)
+    Net::HTTP.start(uri.host, uri.port) do |http|
+      answer = http.get(uri.path)
+      destination = answer['Location'] || answer.body.match(/http-equiv=\"refresh\".*content=\"[0-9]+; url=(.*)\"/i).captures.first.gsub(/(\'|\")/, '')
+      destination == stop ? destination : fetch_redirection(destination, stop)
+    end
+  rescue => e
+    nil
   end
 end
